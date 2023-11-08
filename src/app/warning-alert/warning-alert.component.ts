54d6571e8fbcfa66c71365fcfbd519a33b27759c
tsesme01 <petra.tsesmetzi.ext@ptb.de>
2023-11-08 16:41:32 +0100

    data binding

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>warning-alert works!</p>`,
+  styles: [
+    `
+  p{
+    color:red;
+    font-weight: bold;
+  }
+  `
+]
+})
+export class WarningAlertComponent {
+
+}
